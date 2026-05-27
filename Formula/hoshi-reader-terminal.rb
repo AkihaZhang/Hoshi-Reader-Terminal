cdd5e1f1bbbfb54334403390aee806a05f0b0b13
AkihaZhang <AkihaZhang@users.noreply.github.com>
2026-05-27 11:37:03 +0900

    Fix Homebrew formula for 0.1.3

--- Formula/hoshi-reader-terminal.rb
+++ Formula/hoshi-reader-terminal.rb
@@ -3,8 +3,8 @@ class HoshiReaderTerminal < Formula
 
   desc "Terminal reader inspired by Hoshi Reader"
   homepage "https://github.com/AkihaZhang/Hoshi-Reader-Terminal"
-  url "https://github.com/AkihaZhang/Hoshi-Reader-Terminal/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 :no_check
+  url "https://github.com/AkihaZhang/Hoshi-Reader-Terminal/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "fb6883e2bcc02530427a826f4a96c5fb30aa6b5aa0cf1fc2d4b717daab24c562"
   license "MIT"
 
   depends_on "python@3.12"
@@ -14,6 +14,6 @@ class HoshiReaderTerminal < Formula
   end
 
   test do
-    assert_match "0.1.2", shell_output("#{bin}/hoshi --version")
+    assert_match "0.1.3", shell_output("#{bin}/hoshi --version")
   end
 end
